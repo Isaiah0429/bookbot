8b992bc93a417369ed214666c46c95b1d52af44e
Isaiah0429 <isaiah.miranda0429@icloud.com>
2025-06-22 18:26:03 -0400

    main.py able to print book text into terminal

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath_to_book):
+    with open(filepath_to_book) as book:
+        book_text = book.read()
+    return book_text
+
+def main():
+    filepath_to_book = "books/frankenstein.txt"
+    print(get_book_text(filepath_to_book))
+
+main()
